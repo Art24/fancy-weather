88eb497b9a6d7ed0171745cef6c8a659096d610c
ReaziS <kost9n98@yandex.ru>
2019-11-28 12:55:35 +0300

    refactor: changed cors v2

--- src/js/Model/services/customLocationService.js
+++ src/js/Model/services/customLocationService.js
@@ -4,7 +4,7 @@ class CustomLocationService {
     }
 
     async getCustomLocation(lat, lng) {
-        const response = await fetch((`http://api.timezonedb.com/v2.1/get-time-zone?key=4YRYZRZ0RTST&format=json&by=position&lat=${lat}&lng=${lng}`), {
+        const response = await fetch((`https://api.timezonedb.com/v2.1/get-time-zone?key=4YRYZRZ0RTST&format=json&by=position&lat=${lat}&lng=${lng}`), {
             method: 'GET',
             mode: 'cors',
         });
